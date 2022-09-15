b889455accbaa64506f53e33af2719840921dd47
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2022-09-15 13:22:20 +0900

    코멘트 추가

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import smtplib
 import time
 import os
 
+# 초기 설정
 load_dotenv()
 id = os.getenv('id')
 pw = os.getenv('pw')
@@ -21,6 +22,7 @@ chrome_options.add_argument('window-size=1920x1080')
 chrome_options.add_argument('--start-maximized')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
+
 class Department:
     def __init__(self, path, plist, time):
         self.path = path
@@ -30,17 +32,23 @@ class Department:
         self.rate_path = path + plist[2]
         self.time_path = time
 
+    # 모집 인원
     def accepted(self):
         return driver.find_element(By. XPATH, self.accepted_path).text
 
+    # 지원자 수
     def sent(self):
         return driver.find_element(By. XPATH, self.sent_path).text
 
+    # 경쟁률
     def rate(self):
         return driver.find_element(By. XPATH, self.rate_path).text
 
+    # 기준 시각
     def time(self):
         time_string = driver.find_element(By. XPATH, self.time_path).text
+
+        # 유웨이
         if '분' in time_string:
             time_string = time_string.split('분', 1)[0] + '분'
             time_string = time_string.replace('년 ', '/')
@@ -49,6 +57,7 @@ class Department:
             time_string = time_string.replace('시 ', ':')
             time_string = time_string.replace('분', '')
 
+        # 진학사
         else:
             time_string = time_string.split('현', 1)[0]
             time_string = time_string.replace('-', '/')
@@ -78,6 +87,7 @@ class Department:
 
         return time_string
 
+
 def list_generator(item):
     item_list = [item.accepted(), item.sent(), item.rate(), item.time()]
     return item_list
@@ -89,43 +99,44 @@ while iterations < 6:
 
 tabs = driver.window_handles
 
+# 크롤링
 def get_info():
     col = ['모집 인원', '지원자 수', '경쟁률', '기준 시각']
     ind = ['울산대 지역인재', '부산대 지역인재', '경희대 네오르네상스', '한양대 일반', '고려대 학업우수', '아주대 ACE']
     con = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     df = pd.DataFrame(con, columns=col, index=ind)
 
-    # TAB_1: 울산대 지역인재
+    # TAB_0: 울산대 지역인재
     driver.switch_to.window(tabs[0])
     driver.get('http://ratio.uwayapply.com/Sl5KVzgmQzpKZiUmOiZKcGZUZg==')
     ulsan = Department('//*[@id="Tr_02B02034_002080000"]/td', ['[3]', '[4]', '[5]/font/b'], '//*[@id="ID_DateStr"]/label')
     df.loc['울산대 지역인재'] = list_generator(ulsan)
 
-    # TAB_2: 부산대 지역인재
+    # TAB_1: 부산대 지역인재
     driver.switch_to.window(tabs[1])
     driver.get('http://addon.jinhakapply.com/RatioV1/RatioH/Ratio12100301.html')
     pusan = Department('//*[@id="SelType4F"]/table/tbody/tr[29]/td', ['[3]', '[4]', '[5]'], '//*[@id="RatioTime"]')
     df.loc['부산대 지역인재'] = list_generator(pusan)
 
-    # TAB_3: 경희대 네오르네상스
+    # TAB_2: 경희대 네오르네상스
     driver.switch_to.window(tabs[2])
     driver.get('http://ratio.uwayapply.com/Sl5KOnw5SmYlJjomSnBmVGY=')
     kyunghee = Department('//*[@id="Tr_01312_000700000"]/td', ['[3]', '[4]', '[5]/font/b'], '//*[@id="ID_DateStr"]/label')
     df.loc['경희대 네오르네상스'] = list_generator(kyunghee)
 
-    # TAB_4: 한양대 일반
+    # TAB_3: 한양대 일반
     driver.switch_to.window(tabs[3])
     driver.get('http://addon.jinhakapply.com/RatioV1/RatioH/Ratio11640191.html')
     hanyang = Department('//*[@id="SelType4B"]/table/tbody/tr[23]/td', ['[3]', '[4]', '[5]'], '//*[@id="RatioTime"]')
     df.loc['한양대 일반'] = list_generator(hanyang)
 
-    # TAB_5: 고려대 학업우수
+    # TAB_4: 고려대 학업우수
     driver.switch_to.window(tabs[4])
     driver.get('http://ratio.uwayapply.com/Sl5KOGB9YTlKZiUmOiZKcGZUZg==')
     korea = Department('//*[@id="Tr_0151_000950000"]/td', ['[3]', '[4]', '[5]/font/b'], '//*[@id="ID_DateStr"]/label')
     df.loc['고려대 학업우수'] = list_generator(korea)
 
-    # TAB_6: 아주대 ACE
+    # TAB_5: 아주대 ACE
     driver.switch_to.window(tabs[5])
     driver.get('http://addon.jinhakapply.com/RatioV1/RatioH/Ratio11040291.html')
     ajou = Department('//*[@id="SelType402"]/table/tbody/tr[21]/td', ['[2]', '[3]', '[4]'], '//*[@id="RatioTime"]')
@@ -134,9 +145,10 @@ def get_info():
     return df
 
 
+# push할 HTML
 def push_html(df):
     html_text = '''<head>
-    <meta name="viewport" content="width=device-width" charset="UTF-8" http-equiv="refresh" content="60">
+    <meta name="viewport" content="width=device-width" charset="UTF-8" http-equiv="refresh" content="60" />
     <title>실시간 경쟁률</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -168,6 +180,7 @@ def push_html(df):
     print('Push Completed.')
 
 
+# 최초 실행
 df_before = get_info()
 push_html(df_before)
 
@@ -180,6 +193,7 @@ while True:
         push_html(df_after)
         print('Push Completed.')
 
+        # 메일 내용
         df_before_rmvtime = df_before.drop(columns=['기준 시각'])
         df_after_rmvtime = df_after.drop(columns=['기준 시각'])
         df_diff = df_before_rmvtime.compare(df_after_rmvtime, align_axis=1, keep_shape=False, keep_equal=False)
@@ -192,6 +206,7 @@ while True:
         for item in changes:
             message += item[0] + " - 지원자 수: <b>" + item[1] + "</b>명 → <b>" + item[2] + "</b>명 / 경쟁률: <b>" + item[3] + "</b> → <b>" + item[4] + "</b>\n"
 
+        # 메일 전송
         msg = MIMEText(message, 'html')
         msg["Subject"] = '경쟁률 변경 감지'
         msg["From"] = id
